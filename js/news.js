16ff5e0a0b32bee798c60d9d6da875cb9e9cb615
phucts123456 <hooquangphuc.t4@gmail.com>
2024-04-29 22:15:43 +0700

    add list

--- js/news.js
+++ js/news.js
@@ -1,15 +1,126 @@
-let location_data = {
+let news_list_datas = {
   data: [
     {
-      id: "01",
-      titile: "",
-      content: `<h2 style="text-align: justify;color: rgb(47, 47, 47);background-color: rgb(255, 255, 255);font-size: 16px;">Ocean City, Th&agrave;nh phố điểm đến ph&iacute;a Đ&ocirc;ng H&agrave; Nội tự h&agrave;o được International Business Times (IBTimes)&nbsp;&ndash; Một trong những thời b&aacute;o kinh doanh lớn nhất tại Mỹ&nbsp;c&ugrave;ng nhiều thời b&aacute;o danh gi&aacute; tr&ecirc;n thế giới đ&aacute;nh gi&aacute; l&agrave; Nơi đ&aacute;ng sống bậc nhất h&agrave;nh tinh.</h2>
-<div style="text-align: justify;color: rgb(119, 119, 119);background-color: rgb(255, 255, 255);font-size: 14px;">
-    <p style="text-align: justify;color: rgb(34, 34, 34);font-size: 13px;"><span style="font-size: 12pt;"><span style="font-size: 11pt;"><span style="color: black;">Trải qua h&agrave;nh tr&igrave;nh hơn 5 năm h&igrave;nh th&agrave;nh &amp; ph&aacute;t triển, Ocean City đ&atilde; trở th&agrave;nh <strong><strong>điểm đến hấp dẫn cho c&aacute;c c&ocirc;ng d&acirc;n to&agrave;n cầu</strong></strong></span></span><strong><strong><span style="font-size: 11pt;"><span style="color: black;">&nbsp;sống kết nối, ho&agrave; m&igrave;nh v&agrave;o sự ph&aacute;t triển, đề cao gi&aacute; trị sức khoẻ, tinh thần v&agrave; cộng đồng</span></span></strong></strong><span style="font-size: 11pt;"><span style="color: black;">. Nơi đ&acirc;y hiện</span></span> <span style="font-size: 11pt;"><span style="color: black;">l&agrave; ng&ocirc;i nh&agrave; của hơn <strong><strong>60.000 cư d&acirc;n, gồm cả cư d&acirc;n Việt Nam v&agrave; quốc tế</strong></strong>. Trong đ&oacute;, <strong><strong>cộng đồng cư d&acirc;n H&agrave;n Quốc đang</strong></strong></span></span><strong><strong>&nbsp;</strong></strong><strong><strong><span style="font-size: 11pt;"><span style="color: black;">chiếm số lượng đ&ocirc;ng đảo nhất</span></span></strong></strong><strong><strong>&nbsp;</strong></strong><span style="font-size: 11pt;"><span style="color: black;">trong cộng đồng&nbsp;</span></span><span style="font-size: 11pt;"><span style="color: black;">cư</span></span><span style="font-size: 11pt;"><span style="color: black;">&nbsp;d&acirc;n quốc tế. Đ&oacute; l&agrave; t&iacute;n hiệu đ&aacute;ng mừng cho thấy Ocean City được cộng đồng cư d&acirc;n H&agrave;n Quốc tại Việt Nam đ&oacute;n nhận rất t&iacute;ch cực.</span></span></span></p>
-    <p style="text-align: justify;color: rgb(34, 34, 34);font-size: 13px;">&nbsp;</p>
-    <p style="text-align: justify;color: rgb(34, 34, 34);font-size: 13px;"><span style="font-size: 12pt;"><span style="font-size: 11pt;"><span style="color: black;">Đ&aacute;p lại sự tin y&ecirc;u của cộng đồng H&agrave;n Quốc, trong thời gian qua, Ocean City lu&ocirc;n <strong><strong>đầu tư mạnh mẽ v&agrave;o h&agrave;ng loạt c&aacute;c dịch vụ, tiện &iacute;ch cũng như ch&iacute;nh s&aacute;ch ưu đ&atilde;i d&agrave;nh ri&ecirc;ng cho cư d&acirc;n/kh&aacute;ch h&agrave;ng H&agrave;n Quốc</strong></strong> nhằm mục ti&ecirc;u thu h&uacute;t v&agrave; ph&aacute;t triển cộng đồng H&agrave;n Quốc tại Ocean City.</span></span></span></p>
-    <p style="text-align: justify;color: rgb(34, 34, 34);font-size: 13px;"><img src="https://storage.googleapis.com/vinhomes-data-02/styles/images_160_x_120/public/2022_10/1303_Anh_1_1666754093.jpeg?itok=KisV6NIx" alt=""><br></p>
-</div>`,
+      id: "san-nha-sang-de-dang-voi-chinh-sach-khung-tra-gop-15-nam-khong-lo-lai-suat",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_04/Screenshot 2024-04-09 at 10_1712633265.png?itok=pxhmH55Z",
+      title:
+        "Săn nhà sang dễ dàng với chính sách “khủng”: Trả góp 15 năm không lo lãi suất",
+      content: `Đón đầu đà tăng giá và tranh thủ những chính sách trả góp với ưu đãi tốt là lời giải để không bị bỏ lại phía sau trong cuộc đua sở hữu nhà. Với biên độ sinh lời hấp dẫn trong dài hạn, sở hữu bất`,
+    },
+    {
+      id: "doi-van-tu-thue-tro-thanh-chu-can-ho-hang-sang-nho-biet-tan-dung-goi-vay",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_04/Screenshot 2024-04-09 at 10_1712632823.png?itok=ewsobWgC",
+      title:
+        "‘Đổi vận’ từ thuê trọ thành chủ căn hộ hạng sang nhờ biết tận dụng gói vay",
+      content: `Giữa bối cảnh giá thuê nhà liên tục tăng cao, nhiều người trẻ đã linh hoạt tận dụng ưu đãi thanh toán tốt từ chủ đầu tư để mua nhà. Việc hoán đổi chi phí thuê nhà vào dòng tiền trả góp được`,
+    },
+    {
+      id: "don-chan-song-chu-ky-bat-dong-san-nha-dau-tu-san-lung-nhung-san-pham-co-chinh-sach-khung",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_04/Screenshot 2024-04-09 at 10_1712632539.png?itok=w5zRMstS",
+      title:
+        "Đón “chân sóng” chu kỳ bất động sản, nhà đầu tư săn lùng những sản phẩm có chính sách khủng",
+      content: `Giai đoạn tốt cuối cùng để mua vào của thị trường bất động sảnđang dần đi qua, các chủ đầu tư lại tung ra các chính sách chưa từng có khiến nhiều nhà đầu tư nhanh chóng xuống tiền`,
+    },
+    {
+      id: "kham-pha-chuan-song-khac-biet-tai-ocean-city",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_04/Screenshot 2024-04-09 at 10_1712632323.png?itok=oCnv8InD",
+      title: "Khám phá 'chuẩn sống' khác biệt tại Ocean City",
+      content: `Ocean City đang là nơi an cư đáng mơ ước của hơn 60.000 cư dân cả trong và ngoài nước bởi những giá trị sống khác biệt chỉ có tại “nơi đáng sống nhất hành tinh”.`,
+    },
+    {
+      id: "cu-dan-vinhomes-hao-hưng-tham-gia-loat-hoat-dong-dac-quyen-tai-2-clb-cong-dong",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/club pr1_1711505461.png?itok=XnjVqyCf",
+      title:
+        "Cư dân Vinhomes hào hứng tham gia loạt hoạt động đặc quyền tại 2 CLB cộng đồng",
+      content: `Chỉ sau hơn 1 tháng công bố chiến lược nâng tầm chuẩn sống tại các khu đô thị, các CLB động đồng của Vinhomes với hàng loạt hoạt động, trải nghiệm hấp dẫn đã được đông đảo cư dân đón`,
+    },
+    {
+      id: "cuoc-choi-lon-cua-ty-phu-pham-nhat-vuong-xay-dao-ty-phu-quy-mo-887ha-ban-nha-cho-gioi-thuong-luu-sieu-giau",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/vy%20pr15_1711444514.jpeg?itok=gcuAqcSr",
+      title:
+        "Cuộc chơi lớn của tỷ phú Phạm Nhật Vượng: Xây 'đảo tỷ phú' quy mô 877ha, bán nhà cho giới thượng lưu siêu giàu",
+      content: `Công ty Cổ phần Vinhomes vừa ra mắt dự án “Thành phố Đảo Hoàng Gia” – Vinhomes Royal Island tại đảo Vũ Yên, Hải Phòng - đô thị đảo đầu tiên giữa trung tâm thành phố.`,
+    },
+    {
+      id: "lo-dien-dien-den-an-cu-dau-tu-moi-khien-gioi-nha-giau-quoc-te-dung-ngoi-khong-yen",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/vy pr14_1711443464.jpeg?itok=-Nlpbb14",
+      title: `
+Lộ diện điểm đến an cư, đầu tư mới khiến giới nhà giàu quốc tế “đứng ngồi không yên”`,
+      content: `Sau Dubai, Miami hay Monaco, giới nhà giàu quốc tế đang đổ dồn sự chú ý về Thành phố đảo Hoàng Gia Vinhomes Royal Island - “đảo tỷ phú” vừa ra mắt chính thức ngày 21/3 trên Vinhomes Market - để`,
+    },
+    {
+      id: "co-gi-tai-thanh-pho-dao-hoang-gia-vinhomes-royal-island-vua-ra-mat",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/vy pr13_1711443201.jpeg?itok=qLtzVwVM",
+      title:
+        "Có gì tại Thành phố đảo Hoàng Gia Vinhomes Royal Island vừa ra mắt?",
+      content: `Ngay khi ra mắt trên nền tảng Vinhomes Market, Thành phố đảo Hoàng Gia Vinhomes Royal Island (Hải Phòng) đã gây ấn tượng bởi chính sách bán hàng "độc - lạ" cùng sức hút từ không gian sống và cơ hội “vàng ròng” với giới đầu tư.`,
+    },
+    {
+      id: "vinhomes-royal-island-cong-khai-gia-ben-tren-website",
+      img: `https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/VY%20pr12_1711443028.jpeg?itok=cuhtzD7w`,
+      title: "Vinhomes Royal Island công khai giá bán trên website",
+      content: `Dự án Thành phố Đảo Hoàng Gia được niêm yết giá công khai trên website Vinhomes Market từ 21/3 để phục vụ nhà đầu tư tìm hiểu, tra cứu thông tin.`,
+    },
+    {
+      id: "kham-pha-vien-kim-cuong-tren-vuong-mien-tai-dao-ty-phu-vinhomes-royal-island",
+      img: "https://storage.googleapis.com/vinhomes-data-02/styles/images_263_x_146/public/2024_03/VY%20PR11_1711442745.jpeg?itok=EtMnwryD",
+      title: `Khám phá “viên kim cương trên vương miện” tại “đảo tỷ phú” Vinhomes Royal Island`,
+      content: `Trong bộ sưu tập những tiện ích đẳng cấp thế giới quy tụ tại “đảo tỷ phú” Vinhomes Royal Island (Vũ Yên, Hải Phòng), không thể không nhắc đến River Walk - phố đi bộ văn hóa, vui chơi, mua sắm, ẩm thực bờ sông dài và đẹp nhất Việt Nam.`,
     },
   ],
 };
+
+function create_element(tag_name, attributes) {
+  let element = document.createElement(tag_name);
+
+  if (attributes) {
+    for (let key in attributes) {
+      element[key] = attributes[key];
+    }
+  }
+
+  return element;
+}
+
+let list = document.getElementsByClassName("list")[0];
+
+for (let news_data of news_list_datas.data) {
+  let new_list_wrapper = create_element("div", {
+    className: "new_list_wrapper",
+  });
+  let new_list_content = create_element("div", {
+    className: "new_list_content",
+  });
+  let new_list_content_left = create_element("div", {
+    className: "new_list_content_left",
+  });
+  let new_list_content_left_img_container = create_element("div", {
+    className: "new_list_content_left_img_container",
+  });
+  let new_list_content_right = create_element("div", {
+    className: "new_list_content_right",
+  });
+  let link_img = create_element("a", {
+    href: `chi-tiet.html?title=${news_data.id}`,
+  });
+  let list_item_img = create_element("img", { src: `${news_data.img}` });
+  let heading = create_element("h5");
+  let link_heading = create_element("a", {
+    href: `chi-tiet.html?title=${news_data.id}`,
+    innerText: news_data.title,
+  });
+  let detail_content = create_element("p", {
+    innerText: news_data.content,
+  });
+  new_list_wrapper.appendChild(new_list_content);
+  new_list_content.appendChild(new_list_content_left);
+  new_list_content.appendChild(new_list_content_right);
+  new_list_content_left.appendChild(new_list_content_left_img_container);
+  new_list_content_left_img_container.appendChild(link_img);
+  link_img.appendChild(list_item_img);
+  new_list_content_right.appendChild(heading);
+  heading.appendChild(link_heading);
+  new_list_content_right.appendChild(detail_content);
+  console.log(new_list_wrapper);
+  list.appendChild(new_list_wrapper);
+}
