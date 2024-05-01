e9bb903cd59a08a41a0afe44c251bdd9f8ef07a4
phucts123456 <hooquangphuc.t4@gmail.com>
2024-05-01 19:57:37 +0700

    fix link error

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 let footer_right_form = document.getElementById("footer_right_form");
+console.log(footer_right_form);
 (function () {
   // https://dashboard.emailjs.com/admin/account
   emailjs.init("LVDjy-_AdCjJtPP3e");
@@ -18,22 +19,22 @@ footer_right_form.addEventListener("submit", function (e) {
   );
 });
 
-// let search_icon = document.getElementById("search_icon");
-// let search_input = document.getElementsByClassName("search_input")[0];
-// let input = search_input.getElementsByTagName("input")[0];
+let search_icon = document.getElementById("search_icon");
+let search_input = document.getElementsByClassName("search_input")[0];
+let input = search_input.getElementsByTagName("input")[0];
 
-// search_input.style.display = "none";
+search_input.style.display = "none";
 
-// search_icon.addEventListener("click", function () {
-//   console.log(search_input.style.display);
-//   if (search_input.style.display == "none") {
-//     search_input.style.display = "flex";
-//     console.log("object");
-//   } else {
-//     if (input.value != "") {
-//       window.location.replace(
-//         `http://127.0.0.1:5502/pages/news/tin-tuc.html?title=${input.value}`
-//       );
-//     }
-//   }
-// });
+search_icon.addEventListener("click", function () {
+  console.log(search_input.style.display);
+  if (search_input.style.display == "none") {
+    search_input.style.display = "flex";
+    console.log("object");
+  } else {
+    if (input.value != "") {
+      window.location.replace(
+        `http://127.0.0.1:5502/pages/news/tin-tuc.html?title=${input.value}`
+      );
+    }
+  }
+});
