b5ae84a9012eb8ebee373d0a5ece4b52d23e3c98
phucts123456 <hooquangphuc.t4@gmail.com>
2024-05-04 12:46:23 +0700

    add form email validate

--- js/common.js
+++ js/common.js
@@ -5,22 +5,55 @@ let footer_right_form = document.getElementById("footer_right_form");
 })();
 footer_right_form.addEventListener("submit", function (e) {
   let loading_spinner = document.getElementsByClassName("loadingContainer1")[0];
-  loading_spinner.style.display = "flex";
+
   e.preventDefault();
   const data = new FormData(footer_right_form);
-  emailjs.sendForm("service_xqor18n", "template_z8dxix4", this).then(
-    function () {
-      loading_spinner.style.display = "none";
-      alert(
-        "Cảm ơn bạn đã liên hệ. Chúng tôi sẽ trả lời trong thời gian sớm nhất"
-      );
-    },
-    function (error) {
-      alert("FAILED...", error);
-    }
-  );
+  let error = ValidateForm(data);
+  if (error != "") {
+    alert(error);
+    return;
+  } else {
+    loading_spinner.style.display = "flex";
+    emailjs.sendForm("service_xqor18n", "template_z8dxix4", this).then(
+      function () {
+        loading_spinner.style.display = "none";
+        alert(
+          "Cảm ơn bạn đã liên hệ. Chúng tôi sẽ trả lời trong thời gian sớm nhất"
+        );
+      },
+      function (error) {
+        loading_spinner.style.display = "none";
+        alert("FAILED...", error);
+      }
+    );
+  }
 });
 
+function ValidateForm(data) {
+  let error = "";
+  let newLine = "\r\n";
+  let fullname = data.get("fullname");
+  let phone = data.get("phone");
+  let email = data.get("email");
+  let content = data.get("content");
+  const phoneFormat = new RegExp("(84|0[3|5|7|8|9])+([0-9]{8})");
+
+  const emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g;
+  if (fullname.length <= 0) {
+    error += "Vui lòng nhập tên của bạn" + newLine;
+  }
+  if (!phone.match(phoneFormat)) {
+    error += "Vui lòng nhập đúng format số điện thoại." + newLine;
+  }
+  console.log(email);
+  if (!email.match(emailFormat)) {
+    error += "Vui lòng nhập đúng format email" + newLine;
+  }
+  if (content.length <= 0) {
+    error += "Vui lòng nhập nội dung cần tư vấn" + newLine;
+  }
+  return error;
+}
 let search_icon = document.getElementById("search_icon");
 let search_input = document.getElementsByClassName("search_input")[0];
 let input = search_input.getElementsByTagName("input")[0];
